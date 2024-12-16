a6a034bb87ab5e4ab7e2ba36c3333fbdfa909e1c
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2024-12-15 22:06:50 -0300

    feat: adds conversion between dimension

--- include/paulista/paulista-dimension.hpp
+++ include/paulista/paulista-dimension.hpp
@@ -17,6 +17,11 @@ namespace dimension {
             Meter() : value_(0) {}
             Meter(std::int32_t value) : value_(value) {}
 
+            Meter(const Decimeter& value);
+            Meter(const Centimeter& value);
+            Meter(const Millimeter& value);
+            Meter(const Micrometer& value);
+
             friend std::strong_ordering
             operator<=>(const Meter& lhs, const Meter& rhs) {
                 return lhs.value_ <=> rhs.value_;
@@ -88,9 +93,434 @@ namespace dimension {
             operator<<(std::ostream& os, const Meter& p) {
                 return os << static_cast<std::int32_t>(p.value_) << "m";
             }
+
+            explicit operator std::int32_t() const { return value_; }
+
+            explicit operator Decimeter() const;
+            explicit operator Centimeter() const;
+            explicit operator Millimeter() const;
+            explicit operator Micrometer() const;
         private:
             std::int32_t value_;
     };
+
+    class Decimeter {
+        public:
+            Decimeter() : value_(0) {}
+            Decimeter(std::int32_t value) : value_(value) {}
+
+            Decimeter(const Meter& value);
+            Decimeter(const Centimeter& value);
+            Decimeter(const Millimeter& value);
+            Decimeter(const Micrometer& value);
+
+            friend std::strong_ordering
+            operator<=>(const Decimeter& lhs, const Decimeter& rhs) {
+                return lhs.value_ <=> rhs.value_;
+            }
+
+            friend bool operator==(const Decimeter& lhs, const Decimeter& rhs) = default;
+            friend bool operator!=(const Decimeter& lhs, const Decimeter& rhs) = default;
+            friend bool operator<(const Decimeter& lhs, const Decimeter& rhs) = default;
+            friend bool operator<=(const Decimeter& lhs, const Decimeter& rhs) = default;
+            friend bool operator>(const Decimeter& lhs, const Decimeter& rhs) = default;
+            friend bool operator>=(const Decimeter& lhs, const Decimeter& rhs) = default;
+
+            Decimeter&
+            operator+=(const Decimeter& rhs) {
+                value_ += rhs.value_; return *this;
+            }
+
+            Decimeter&
+            operator-=(const Decimeter& rhs) {
+                value_ -= rhs.value_; return *this;
+            }
+
+            Decimeter&
+            operator*=(int32_t value) {
+                value_ *= value;
+                return *this;
+            }
+
+            Decimeter&
+            operator/=(int32_t value) {
+                value_ /= value;
+                return *this;
+            }
+
+            friend Decimeter
+            operator+(Decimeter lhs, const Decimeter& rhs) {
+                lhs += rhs; return lhs;
+            }
+
+            friend Decimeter
+            operator-(Decimeter lhs, const Decimeter& rhs) {
+                lhs -= rhs; return lhs;
+            }
+
+            friend Decimeter
+            operator-(const Decimeter& lhs) {
+                Decimeter result;
+                result.value_ -= lhs.value_;
+
+                return result;
+            }
+
+            friend Decimeter
+            operator*(std::int32_t value, Decimeter p) {
+                p *= value; return p;
+            }
+
+            friend Decimeter
+            operator*(Decimeter p, std::int32_t value) {
+                p *= value; return p;
+            }
+
+            friend Decimeter
+            operator/(Decimeter p, std::int32_t value) {
+                p /= value; return p;
+            }
+
+            friend std::ostream&
+            operator<<(std::ostream& os, const Decimeter& p) {
+                return os << static_cast<std::int32_t>(p.value_) << "dm";
+            }
+
+            explicit operator std::int32_t() const { return value_; }
+
+            explicit operator Meter() const;
+            explicit operator Centimeter() const;
+            explicit operator Millimeter() const;
+            explicit operator Micrometer() const;
+        private:
+            std::int32_t value_;
+    };
+
+    class Centimeter {
+        public:
+            Centimeter() : value_(0) {}
+            Centimeter(std::int32_t value) : value_(value) {}
+
+            Centimeter(const Meter& value);
+            Centimeter(const Decimeter& value);
+            Centimeter(const Millimeter& value);
+            Centimeter(const Micrometer& value);
+
+            friend std::strong_ordering
+            operator<=>(const Centimeter& lhs, const Centimeter& rhs) {
+                return lhs.value_ <=> rhs.value_;
+            }
+
+            friend bool operator==(const Centimeter& lhs, const Centimeter& rhs) = default;
+            friend bool operator!=(const Centimeter& lhs, const Centimeter& rhs) = default;
+            friend bool operator<(const Centimeter& lhs, const Centimeter& rhs) = default;
+            friend bool operator<=(const Centimeter& lhs, const Centimeter& rhs) = default;
+            friend bool operator>(const Centimeter& lhs, const Centimeter& rhs) = default;
+            friend bool operator>=(const Centimeter& lhs, const Centimeter& rhs) = default;
+
+            Centimeter&
+            operator+=(const Centimeter& rhs) {
+                value_ += rhs.value_; return *this;
+            }
+
+            Centimeter&
+            operator-=(const Centimeter& rhs) {
+                value_ -= rhs.value_; return *this;
+            }
+
+            Centimeter&
+            operator*=(int32_t value) {
+                value_ *= value;
+                return *this;
+            }
+
+            Centimeter&
+            operator/=(int32_t value) {
+                value_ /= value;
+                return *this;
+            }
+
+            friend Centimeter
+            operator+(Centimeter lhs, const Centimeter& rhs) {
+                lhs += rhs; return lhs;
+            }
+
+            friend Centimeter
+            operator-(Centimeter lhs, const Centimeter& rhs) {
+                lhs -= rhs; return lhs;
+            }
+
+            friend Centimeter
+            operator-(const Centimeter& lhs) {
+                Centimeter result;
+                result.value_ -= lhs.value_;
+
+                return result;
+            }
+
+            friend Centimeter
+            operator*(std::int32_t value, Centimeter p) {
+                p *= value; return p;
+            }
+
+            friend Centimeter
+            operator*(Centimeter p, std::int32_t value) {
+                p *= value; return p;
+            }
+
+            friend Centimeter
+            operator/(Centimeter p, std::int32_t value) {
+                p /= value; return p;
+            }
+
+            friend std::ostream&
+            operator<<(std::ostream& os, const Centimeter& p) {
+                return os << static_cast<std::int32_t>(p.value_) << "cm";
+            }
+
+            explicit operator std::int32_t() const { return value_; }
+
+            explicit operator Meter() const;
+            explicit operator Decimeter() const;
+            explicit operator Millimeter() const;
+            explicit operator Micrometer() const;
+        private:
+            std::int32_t value_;
+    };
+
+    class Millimeter {
+        public:
+            Millimeter() : value_(0) {}
+            Millimeter(std::int32_t value) : value_(value) {}
+
+            Millimeter(const Meter& value);
+            Millimeter(const Decimeter& value);
+            Millimeter(const Centimeter& value);
+            Millimeter(const Micrometer& value);
+
+            friend std::strong_ordering
+            operator<=>(const Millimeter& lhs, const Millimeter& rhs) {
+                return lhs.value_ <=> rhs.value_;
+            }
+
+            friend bool operator==(const Millimeter& lhs, const Millimeter& rhs) = default;
+            friend bool operator!=(const Millimeter& lhs, const Millimeter& rhs) = default;
+            friend bool operator<(const Millimeter& lhs, const Millimeter& rhs) = default;
+            friend bool operator<=(const Millimeter& lhs, const Millimeter& rhs) = default;
+            friend bool operator>(const Millimeter& lhs, const Millimeter& rhs) = default;
+            friend bool operator>=(const Millimeter& lhs, const Millimeter& rhs) = default;
+
+            Millimeter&
+            operator+=(const Millimeter& rhs) {
+                value_ += rhs.value_; return *this;
+            }
+
+            Millimeter&
+            operator-=(const Millimeter& rhs) {
+                value_ -= rhs.value_; return *this;
+            }
+
+            Millimeter&
+            operator*=(int32_t value) {
+                value_ *= value;
+                return *this;
+            }
+
+            Millimeter&
+            operator/=(int32_t value) {
+                value_ /= value;
+                return *this;
+            }
+
+            friend Millimeter
+            operator+(Millimeter lhs, const Millimeter& rhs) {
+                lhs += rhs; return lhs;
+            }
+
+            friend Millimeter
+            operator-(Millimeter lhs, const Millimeter& rhs) {
+                lhs -= rhs; return lhs;
+            }
+
+            friend Millimeter
+            operator-(const Millimeter& lhs) {
+                Millimeter result;
+                result.value_ -= lhs.value_;
+
+                return result;
+            }
+
+            friend Millimeter
+            operator*(std::int32_t value, Millimeter p) {
+                p *= value; return p;
+            }
+
+            friend Millimeter
+            operator*(Millimeter p, std::int32_t value) {
+                p *= value; return p;
+            }
+
+            friend Millimeter
+            operator/(Millimeter p, std::int32_t value) {
+                p /= value; return p;
+            }
+
+            friend std::ostream&
+            operator<<(std::ostream& os, const Millimeter& p) {
+                return os << static_cast<std::int32_t>(p.value_) << "mm";
+            }
+
+            explicit operator std::int32_t() const { return value_; }
+
+            explicit operator Meter() const;
+            explicit operator Decimeter() const;
+            explicit operator Centimeter() const;
+            explicit operator Micrometer() const;
+        private:
+            std::int32_t value_;
+    };
+
+    class Micrometer {
+        public:
+            Micrometer() : value_(0) {}
+            Micrometer(std::int32_t value) : value_(value) {}
+
+            Micrometer(const Meter& value);
+            Micrometer(const Decimeter& value);
+            Micrometer(const Centimeter& value);
+            Micrometer(const Millimeter& value);
+
+            friend std::strong_ordering
+            operator<=>(const Micrometer& lhs, const Micrometer& rhs) {
+                return lhs.value_ <=> rhs.value_;
+            }
+
+            friend bool operator==(const Micrometer& lhs, const Micrometer& rhs) = default;
+            friend bool operator!=(const Micrometer& lhs, const Micrometer& rhs) = default;
+            friend bool operator<(const Micrometer& lhs, const Micrometer& rhs) = default;
+            friend bool operator<=(const Micrometer& lhs, const Micrometer& rhs) = default;
+            friend bool operator>(const Micrometer& lhs, const Micrometer& rhs) = default;
+            friend bool operator>=(const Micrometer& lhs, const Micrometer& rhs) = default;
+
+            Micrometer&
+            operator+=(const Micrometer& rhs) {
+                value_ += rhs.value_; return *this;
+            }
+
+            Micrometer&
+            operator-=(const Micrometer& rhs) {
+                value_ -= rhs.value_; return *this;
+            }
+
+            Micrometer&
+            operator*=(int32_t value) {
+                value_ *= value;
+                return *this;
+            }
+
+            Micrometer&
+            operator/=(int32_t value) {
+                value_ /= value;
+                return *this;
+            }
+
+            friend Micrometer
+            operator+(Micrometer lhs, const Micrometer& rhs) {
+                lhs += rhs; return lhs;
+            }
+
+            friend Micrometer
+            operator-(Micrometer lhs, const Micrometer& rhs) {
+                lhs -= rhs; return lhs;
+            }
+
+            friend Micrometer
+            operator-(const Micrometer& lhs) {
+                Micrometer result;
+                result.value_ -= lhs.value_;
+
+                return result;
+            }
+
+            friend Micrometer
+            operator*(std::int32_t value, Micrometer p) {
+                p *= value; return p;
+            }
+
+            friend Micrometer
+            operator*(Micrometer p, std::int32_t value) {
+                p *= value; return p;
+            }
+
+            friend Micrometer
+            operator/(Micrometer p, std::int32_t value) {
+                p /= value; return p;
+            }
+
+            friend std::ostream&
+            operator<<(std::ostream& os, const Micrometer& p) {
+                return os << static_cast<std::int32_t>(p.value_) << "um";
+            }
+
+            explicit operator std::int32_t() const { return value_; }
+
+            explicit operator Meter() const;
+            explicit operator Decimeter() const;
+            explicit operator Centimeter() const;
+            explicit operator Millimeter() const;
+        private:
+            std::int32_t value_;
+    };
+
+    inline Meter::Meter(const Decimeter&  value) : value_(static_cast<std::int32_t>(value) / 1e1){}
+    inline Meter::Meter(const Centimeter& value) : value_(static_cast<std::int32_t>(value) / 1e2){}
+    inline Meter::Meter(const Millimeter& value) : value_(static_cast<std::int32_t>(value) / 1e3){}
+    inline Meter::Meter(const Micrometer& value) : value_(static_cast<std::int32_t>(value) / 1e6){}
+
+    inline Decimeter::Decimeter(const Meter&      value) : value_(static_cast<std::int32_t>(value) * 1e1){}
+    inline Decimeter::Decimeter(const Centimeter& value) : value_(static_cast<std::int32_t>(value) / 1e1){}
+    inline Decimeter::Decimeter(const Millimeter& value) : value_(static_cast<std::int32_t>(value) / 1e2){}
+    inline Decimeter::Decimeter(const Micrometer& value) : value_(static_cast<std::int32_t>(value) / 1e5){}
+
+    inline Centimeter::Centimeter(const Meter&      value) : value_(static_cast<std::int32_t>(value) * 1e2){}
+    inline Centimeter::Centimeter(const Decimeter&  value) : value_(static_cast<std::int32_t>(value) * 1e1){}
+    inline Centimeter::Centimeter(const Millimeter& value) : value_(static_cast<std::int32_t>(value) / 1e1){}
+    inline Centimeter::Centimeter(const Micrometer& value) : value_(static_cast<std::int32_t>(value) / 1e4){}
+
+    inline Millimeter::Millimeter(const Meter&      value) : value_(static_cast<std::int32_t>(value) * 1e3){}
+    inline Millimeter::Millimeter(const Decimeter&  value) : value_(static_cast<std::int32_t>(value) * 1e2){}
+    inline Millimeter::Millimeter(const Centimeter& value) : value_(static_cast<std::int32_t>(value) * 1e1){}
+    inline Millimeter::Millimeter(const Micrometer& value) : value_(static_cast<std::int32_t>(value) / 1e3){}
+
+    inline Micrometer::Micrometer(const Meter&      value) : value_(static_cast<std::int32_t>(value) * 1e6){}
+    inline Micrometer::Micrometer(const Decimeter&  value) : value_(static_cast<std::int32_t>(value) * 1e5){}
+    inline Micrometer::Micrometer(const Centimeter& value) : value_(static_cast<std::int32_t>(value) * 1e4){}
+    inline Micrometer::Micrometer(const Millimeter& value) : value_(static_cast<std::int32_t>(value) * 1e3){}
+
+    inline Meter::operator Decimeter() const    { return Decimeter(*this); }
+    inline Meter::operator Centimeter() const   { return Centimeter(*this); }
+    inline Meter::operator Millimeter() const   { return Millimeter(*this); }
+    inline Meter::operator Micrometer() const   { return Micrometer(*this); }
+
+    inline Decimeter::operator Meter() const        { return Meter(*this); }
+    inline Decimeter::operator Centimeter() const   { return Centimeter(*this); }
+    inline Decimeter::operator Millimeter() const   { return Millimeter(*this); }
+    inline Decimeter::operator Micrometer() const   { return Micrometer(*this); }
+
+    inline Centimeter::operator Meter() const       { return Meter(*this); }
+    inline Centimeter::operator Decimeter() const   { return Decimeter(*this); }
+    inline Centimeter::operator Millimeter() const  { return Millimeter(*this); }
+    inline Centimeter::operator Micrometer() const  { return Micrometer(*this); }
+
+    inline Millimeter::operator Meter() const       { return Meter(*this); }
+    inline Millimeter::operator Decimeter() const   { return Decimeter(*this); }
+    inline Millimeter::operator Centimeter() const  { return Centimeter(*this); }
+    inline Millimeter::operator Micrometer() const  { return Micrometer(*this); }
+
+    inline Micrometer::operator Meter() const       { return Meter(*this); }
+    inline Micrometer::operator Decimeter() const   { return Decimeter(*this); }
+    inline Micrometer::operator Centimeter() const  { return Centimeter(*this); }
+    inline Micrometer::operator Millimeter() const  { return Millimeter(*this); }
 } // namespace dimension
 } // namespace paulista
 
--- tests/dimension.cpp
+++ tests/dimension.cpp
@@ -3,6 +3,10 @@
 #include <rapidcheck/gtest.h>
 
 using paulista::dimension::Meter;
+using paulista::dimension::Decimeter;
+using paulista::dimension::Centimeter;
+using paulista::dimension::Millimeter;
+using paulista::dimension::Micrometer;
 
 namespace rc {
     template<>
@@ -12,9 +16,41 @@ namespace rc {
             return gen::construct<Meter>(gen::inRange(-10, 10));
         }
     };
+
+    template<>
+    struct Arbitrary<Decimeter> {
+        static Gen<Decimeter>
+        arbitrary() {
+            return gen::construct<Decimeter>(gen::inRange(-100, 100));
+        }
+    };
+
+    template<>
+    struct Arbitrary<Centimeter> {
+        static Gen<Centimeter>
+        arbitrary() {
+            return gen::construct<Centimeter>(gen::inRange(-1000, 1000));
+        }
+    };
+
+    template<>
+    struct Arbitrary<Millimeter> {
+        static Gen<Millimeter>
+        arbitrary() {
+            return gen::construct<Millimeter>(gen::inRange(-10000, 10000));
+        }
+    };
+
+    template<>
+    struct Arbitrary<Micrometer> {
+        static Gen<Micrometer>
+        arbitrary() {
+            return gen::construct<Micrometer>(gen::inRange(-100000, 100000));
+        }
+    };
 }
 
-TEST(COMPARISON, _) {
+TEST(METER, COMPARISON) {
     Meter zero;
     Meter one(1);
 
@@ -31,48 +67,277 @@ TEST(COMPARISON, _) {
     ASSERT_GT( one, zero);
 }
 
-RC_GTEST_PROP(ADD, NEUTRAL, (const Meter& xs)) {
+RC_GTEST_PROP(METER, ADD, (const Meter& x, const Meter& y)) {
     Meter zero;
-    EXPECT_EQ(xs + zero, xs);
+    Meter successor(1);
+
+    EXPECT_EQ(x + zero, x);         // NEUTRAL
+    EXPECT_EQ(x + y, y + x);        // COMMUTATIVE
+    EXPECT_NE(x + successor, x);    // SUCCESSOR
 }
 
-RC_GTEST_PROP(ADD, COMMUTATIVE, (const Meter& xs, const Meter& ys)) {
-    EXPECT_EQ(xs + ys, ys + xs);
-}
-
-RC_GTEST_PROP(ADD, SUCCESSOR, (const Meter& xs)) {
-    EXPECT_NE(xs + Meter(1), xs);
-}
-
-RC_GTEST_PROP(SUBTRACT, NEUTRAL, (const Meter& xs)) {
+RC_GTEST_PROP(METER, SUBTRACT, (const Meter& x, const Meter& y)) {
     Meter zero;
-    EXPECT_EQ(xs - zero, xs);
+    Meter successor(1);
+
+    EXPECT_EQ(x - zero, x);     // NEUTRAL
+    EXPECT_EQ(x - y, -(y - x)); // ANTICOMMUTATIVE
 }
 
-RC_GTEST_PROP(SUBTRACT, ANTICOMMUTATIVE, (const Meter& xs, const Meter& ys)) {
-    EXPECT_EQ(xs - ys, -(ys - xs));
-}
-
-RC_GTEST_PROP(MULTIPLICATION, NEUTRAL, (const Meter& xs)) {
-    EXPECT_EQ(xs * 1, xs);
-    EXPECT_EQ(1 * xs, xs);
-}
-
-RC_GTEST_PROP(MULTIPLICATION, ZERO, (const Meter& xs)) {
+RC_GTEST_PROP(METER, MULTIPLICATION, (const Meter& x)) {
     Meter zero;
-    EXPECT_EQ(xs * 0, zero);
+
+    EXPECT_EQ(x * 1, x);        // NEUTRAL
+    EXPECT_EQ(1 * x, x);        // NEUTRAL
+    EXPECT_EQ(x * 0, zero);     // ZERO
+    EXPECT_EQ(x * 2, x + x);    // DOUBLE
+    EXPECT_EQ(x * 3, 3 * x);    // COMMUTATIVE
 }
 
-RC_GTEST_PROP(MULTIPLICATION, DOUBLE, (const Meter& xs)) {
-    EXPECT_EQ(xs * 2, xs + xs);
+RC_GTEST_PROP(METER, DIVISION, (const Meter& x)) {
+    EXPECT_EQ(x / 1, x);
 }
 
-RC_GTEST_PROP(MULTIPLICATION, COMMUTATIVE, (const Meter& xs, std::uint8_t v)) {
-    EXPECT_EQ(xs * v, v * xs);
+TEST(METER, CONVERSION) {
+    Meter unit(1e0);
+
+    EXPECT_EQ(1e0, static_cast<std::int32_t>(           unit ));
+    EXPECT_EQ(1e1, static_cast<std::int32_t>( Decimeter(unit)));
+    EXPECT_EQ(1e2, static_cast<std::int32_t>(Centimeter(unit)));
+    EXPECT_EQ(1e3, static_cast<std::int32_t>(Millimeter(unit)));
+    EXPECT_EQ(1e6, static_cast<std::int32_t>(Micrometer(unit)));
 }
 
-RC_GTEST_PROP(DIVISION, NEUTRAL, (const Meter& xs)) {
-    EXPECT_EQ(xs / 1, xs);
+TEST(DECIMETER, COMPARISON) {
+    Decimeter zero;
+    Decimeter one(1);
+
+    ASSERT_EQ(zero, zero);
+    ASSERT_EQ( one,  one);
+
+    ASSERT_NE(zero,  one);
+    ASSERT_LE(zero, zero);
+    ASSERT_LE(zero,  one);
+    ASSERT_LT(zero,  one);
+
+    ASSERT_GE(zero, zero);
+    ASSERT_GE( one, zero);
+    ASSERT_GT( one, zero);
+}
+
+RC_GTEST_PROP(DECIMETER, ADD, (const Decimeter& x, const Decimeter& y)) {
+    Decimeter zero;
+    Decimeter successor(1);
+
+    EXPECT_EQ(x + zero, x);         // NEUTRAL
+    EXPECT_EQ(x + y, y + x);        // COMMUTATIVE
+    EXPECT_NE(x + successor, x);    // SUCCESSOR
+}
+
+RC_GTEST_PROP(DECIMETER, SUBTRACT, (const Decimeter& x, const Decimeter& y)) {
+    Decimeter zero;
+    Decimeter successor(1);
+
+    EXPECT_EQ(x - zero, x);     // NEUTRAL
+    EXPECT_EQ(x - y, -(y - x)); // ANTICOMMUTATIVE
+}
+
+RC_GTEST_PROP(DECIMETER, MULTIPLICATION, (const Decimeter& x)) {
+    Decimeter zero;
+
+    EXPECT_EQ(x * 1, x);        // NEUTRAL
+    EXPECT_EQ(1 * x, x);        // NEUTRAL
+    EXPECT_EQ(x * 0, zero);     // ZERO
+    EXPECT_EQ(x * 2, x + x);    // DOUBLE
+    EXPECT_EQ(x * 3, 3 * x);    // COMMUTATIVE
+}
+
+RC_GTEST_PROP(DECIMETER, DIVISION, (const Decimeter& x)) {
+    EXPECT_EQ(x / 1, x);
+}
+
+TEST(DECIMETER, CONVERSION) {
+    Decimeter unit(1e1);
+
+    EXPECT_EQ(1e0, static_cast<std::int32_t>(     Meter(unit)));
+    EXPECT_EQ(1e1, static_cast<std::int32_t>(           unit));
+    EXPECT_EQ(1e2, static_cast<std::int32_t>(Centimeter(unit)));
+    EXPECT_EQ(1e3, static_cast<std::int32_t>(Millimeter(unit)));
+    EXPECT_EQ(1e6, static_cast<std::int32_t>(Micrometer(unit)));
+}
+
+TEST(CENTIMETER, COMPARISON) {
+    Centimeter zero;
+    Centimeter one(1);
+
+    ASSERT_EQ(zero, zero);
+    ASSERT_EQ( one,  one);
+
+    ASSERT_NE(zero,  one);
+    ASSERT_LE(zero, zero);
+    ASSERT_LE(zero,  one);
+    ASSERT_LT(zero,  one);
+
+    ASSERT_GE(zero, zero);
+    ASSERT_GE( one, zero);
+    ASSERT_GT( one, zero);
+}
+
+RC_GTEST_PROP(CENTIMETER, ADD, (const Centimeter& x, const Centimeter& y)) {
+    Centimeter zero;
+    Centimeter successor(1);
+
+    EXPECT_EQ(x + zero, x);         // NEUTRAL
+    EXPECT_EQ(x + y, y + x);        // COMMUTATIVE
+    EXPECT_NE(x + successor, x);    // SUCCESSOR
+}
+
+RC_GTEST_PROP(CENTIMETER, SUBTRACT, (const Centimeter& x, const Centimeter& y)) {
+    Centimeter zero;
+    Centimeter successor(1);
+
+    EXPECT_EQ(x - zero, x);     // NEUTRAL
+    EXPECT_EQ(x - y, -(y - x)); // ANTICOMMUTATIVE
+}
+
+RC_GTEST_PROP(CENTIMETER, MULTIPLICATION, (const Centimeter& x)) {
+    Centimeter zero;
+
+    EXPECT_EQ(x * 1, x);        // NEUTRAL
+    EXPECT_EQ(1 * x, x);        // NEUTRAL
+    EXPECT_EQ(x * 0, zero);     // ZERO
+    EXPECT_EQ(x * 2, x + x);    // DOUBLE
+    EXPECT_EQ(x * 3, 3 * x);    // COMMUTATIVE
+}
+
+RC_GTEST_PROP(CENTIMETER, DIVISION, (const Centimeter& x)) {
+    EXPECT_EQ(x / 1, x);
+}
+
+TEST(CENTIMETER, CONVERSION) {
+    Centimeter unit(1e2);
+
+    EXPECT_EQ(1e0, static_cast<std::int32_t>(     Meter(unit)));
+    EXPECT_EQ(1e1, static_cast<std::int32_t>( Decimeter(unit)));
+    EXPECT_EQ(1e2, static_cast<std::int32_t>(            unit));
+    EXPECT_EQ(1e3, static_cast<std::int32_t>(Millimeter(unit)));
+    EXPECT_EQ(1e6, static_cast<std::int32_t>(Micrometer(unit)));
+}
+
+TEST(MILLIMETER, COMPARISON) {
+    Millimeter zero;
+    Millimeter one(1);
+
+    ASSERT_EQ(zero, zero);
+    ASSERT_EQ( one,  one);
+
+    ASSERT_NE(zero,  one);
+    ASSERT_LE(zero, zero);
+    ASSERT_LE(zero,  one);
+    ASSERT_LT(zero,  one);
+
+    ASSERT_GE(zero, zero);
+    ASSERT_GE( one, zero);
+    ASSERT_GT( one, zero);
+}
+
+RC_GTEST_PROP(MILLIMETER, ADD, (const Millimeter& x, const Millimeter& y)) {
+    Millimeter zero;
+    Millimeter successor(1);
+
+    EXPECT_EQ(x + zero, x);         // NEUTRAL
+    EXPECT_EQ(x + y, y + x);        // COMMUTATIVE
+    EXPECT_NE(x + successor, x);    // SUCCESSOR
+}
+
+RC_GTEST_PROP(MILLIMETER, SUBTRACT, (const Millimeter& x, const Millimeter& y)) {
+    Millimeter zero;
+    Millimeter successor(1);
+
+    EXPECT_EQ(x - zero, x);     // NEUTRAL
+    EXPECT_EQ(x - y, -(y - x)); // ANTICOMMUTATIVE
+}
+
+RC_GTEST_PROP(MILLIMETER, MULTIPLICATION, (const Millimeter& x)) {
+    Millimeter zero;
+
+    EXPECT_EQ(x * 1, x);        // NEUTRAL
+    EXPECT_EQ(1 * x, x);        // NEUTRAL
+    EXPECT_EQ(x * 0, zero);     // ZERO
+    EXPECT_EQ(x * 2, x + x);    // DOUBLE
+    EXPECT_EQ(x * 3, 3 * x);    // COMMUTATIVE
+}
+
+RC_GTEST_PROP(MILLIMETER, DIVISION, (const Millimeter& x)) {
+    EXPECT_EQ(x / 1, x);
+}
+
+TEST(MILLIMETER, CONVERSION) {
+    Millimeter unit(1e3);
+
+    EXPECT_EQ(1e0, static_cast<std::int32_t>(     Meter(unit)));
+    EXPECT_EQ(1e1, static_cast<std::int32_t>( Decimeter(unit)));
+    EXPECT_EQ(1e2, static_cast<std::int32_t>(Centimeter(unit)));
+    EXPECT_EQ(1e3, static_cast<std::int32_t>(            unit));
+    EXPECT_EQ(1e6, static_cast<std::int32_t>(Micrometer(unit)));
+}
+
+TEST(MICROMETER, COMPARISON) {
+    Micrometer zero;
+    Micrometer one(1);
+
+    ASSERT_EQ(zero, zero);
+    ASSERT_EQ( one,  one);
+
+    ASSERT_NE(zero,  one);
+    ASSERT_LE(zero, zero);
+    ASSERT_LE(zero,  one);
+    ASSERT_LT(zero,  one);
+
+    ASSERT_GE(zero, zero);
+    ASSERT_GE( one, zero);
+    ASSERT_GT( one, zero);
+}
+
+RC_GTEST_PROP(MICROMETER, ADD, (const Micrometer& x, const Micrometer& y)) {
+    Micrometer zero;
+    Micrometer successor(1);
+
+    EXPECT_EQ(x + zero, x);         // NEUTRAL
+    EXPECT_EQ(x + y, y + x);        // COMMUTATIVE
+    EXPECT_NE(x + successor, x);    // SUCCESSOR
+}
+
+RC_GTEST_PROP(MICROMETER, SUBTRACT, (const Micrometer& x, const Micrometer& y)) {
+    Micrometer zero;
+    Micrometer successor(1);
+
+    EXPECT_EQ(x - zero, x);     // NEUTRAL
+    EXPECT_EQ(x - y, -(y - x)); // ANTICOMMUTATIVE
+}
+
+RC_GTEST_PROP(MICROMETER, MULTIPLICATION, (const Micrometer& x)) {
+    Micrometer zero;
+
+    EXPECT_EQ(x * 1, x);        // NEUTRAL
+    EXPECT_EQ(1 * x, x);        // NEUTRAL
+    EXPECT_EQ(x * 0, zero);     // ZERO
+    EXPECT_EQ(x * 2, x + x);    // DOUBLE
+    EXPECT_EQ(x * 3, 3 * x);    // COMMUTATIVE
+}
+
+RC_GTEST_PROP(MICROMETER, DIVISION, (const Micrometer& x)) {
+    EXPECT_EQ(x / 1, x);
+}
+
+TEST(MICROMETER, CONVERSION) {
+    Micrometer unit(1e6);
+
+    EXPECT_EQ(1e0, static_cast<std::int32_t>(     Meter(unit)));
+    EXPECT_EQ(1e1, static_cast<std::int32_t>( Decimeter(unit)));
+    EXPECT_EQ(1e2, static_cast<std::int32_t>(Centimeter(unit)));
+    EXPECT_EQ(1e3, static_cast<std::int32_t>(Millimeter(unit)));
+    EXPECT_EQ(1e6, static_cast<std::int32_t>(            unit));
 }
 
 int
