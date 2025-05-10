0450a663f61605d65b447316b22628bd546b7df9
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2025-05-10 17:48:15 -0300

    refactor: removes collision (gjk) related code

--- include/paulista/paulista.hpp
+++ include/paulista/paulista.hpp
@@ -1,9 +1,7 @@
 #ifndef PAULISTA_HPP__
 #define PAULISTA_HPP__
 
-// #include "paulista-collision.hpp"
 #include "paulista-dimension.hpp"
 #include "paulista-point.hpp"
-// #include "paulista-simplex.hpp"
 
 #endif // PAULISTA_HPP__
