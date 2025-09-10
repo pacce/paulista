587013dd162756d86326c0cee99c1c3670b25acb
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2025-09-10 00:03:12 -0300

    feat: adds centroid estimate for elements

--- include/paulista/paulista-domain.hpp
+++ include/paulista/paulista-domain.hpp
@@ -7,12 +7,17 @@
 
 namespace paulista {
 namespace domain {
+    template <typename Precision> using Node        = paulista::geometry::tridimensional::Point<Precision>;
+    template <typename Precision> using Nodes       = std::vector<Node<Precision>>;
+
+    template <typename Precision> using Centroid    = paulista::geometry::tridimensional::Point<Precision>;
+    template <typename Precision> using Centroids   = std::vector<Node<Precision>>;
 namespace element {
 namespace visitor {
     template <typename T>
     struct is_element {
         template <typename U>
-        bool operator()(const U& shape) const {
+        bool operator()(const U&) const {
             return std::is_same_v<T, U>;
         }
     };
@@ -21,6 +26,17 @@ namespace visitor {
         std::size_t u;
         std::size_t v;
         std::size_t w;
+
+        template <typename T>
+        std::optional<Centroid<T>>
+        centroid(const Nodes<T>& ns) const {
+            if (u >= ns.size()) { return std::nullopt; }
+            if (v >= ns.size()) { return std::nullopt; }
+            if (w >= ns.size()) { return std::nullopt; }
+
+            Nodes<T> ps = {ns[u], ns[v], ns[w]};
+            return paulista::geometry::tridimensional::point::centroid(ps);
+        }
     };
 
     struct Tetrahedron {
@@ -28,13 +44,23 @@ namespace visitor {
         std::size_t v;
         std::size_t w;
         std::size_t z;
+
+        template <typename T>
+        std::optional<Centroid<T>>
+        centroid(const Nodes<T>& ns) const {
+            if (u >= ns.size()) { return std::nullopt; }
+            if (v >= ns.size()) { return std::nullopt; }
+            if (w >= ns.size()) { return std::nullopt; }
+            if (z >= ns.size()) { return std::nullopt; }
+
+            Nodes<T> ps = {ns[u], ns[v], ns[w], ns[z]};
+            return paulista::geometry::tridimensional::point::centroid(ps);
+        }
     };
 
     constexpr visitor::is_element<Triangle>     is_triangle;
     constexpr visitor::is_element<Tetrahedron>  is_tetrahedron;
 } // namespace element
-    template <typename Precision>
-    using Nodes = std::vector<paulista::geometry::tridimensional::Point<Precision>>;
 
     using Element = std::variant<
         element::Triangle, 
